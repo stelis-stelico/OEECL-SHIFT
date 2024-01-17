0a2be1fdd9e7f026ddfa01c06ffc05311bb2b6b1
stelis-stelico <121448105+stelis-stelico@users.noreply.github.com>
2024-01-17 15:54:47 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -14,13 +14,13 @@ function predictShift() {
         case "A":
             result = "Shift A ";
         if (cycleDay < 4) {
-            result += `is on day ${cycleDay + 5} of off duty`;
+            result += `(Day ${cycleDay + 5} off duty)`;
         } else if (cycleDay < 8) {
-            result += `is on day ${cycleDay - 3} of morning duty`;
+            result += `(Day ${cycleDay - 3} morning duty)`;
         } else if (cycleDay < 12) {
-            result += `is on day ${cycleDay - 7} of night duty`;
+            result += `(Day ${cycleDay - 7} night duty)`;
         } else {
-            result += `is on day ${cycleDay - 11} of off duty`;
+            result += `(Day ${cycleDay - 11} off duty)`;
         }
         break;
           
@@ -28,13 +28,13 @@ function predictShift() {
         case "B":
         result = "Shift B ";
         if (cycleDay < 4) {
-            result += `is on day ${cycleDay + 1} of night duty`;
+            result += `(Day ${cycleDay + 1} night duty)`;
         } else if (cycleDay < 8) {
-            result += `is on day ${cycleDay - 3} of off duty`;
+            result += `(Day ${cycleDay - 3} off duty)`;
         } else if (cycleDay < 12) {
-            result += `is on ${cycleDay - 3} of off duty`;
+            result += `(Day ${cycleDay - 3} off duty)`;
         } else {
-            result += `is on day ${cycleDay - 11} of morning duty`;
+            result += `(Day ${cycleDay - 11} morning duty)`;
         }
         break;
 
@@ -42,24 +42,24 @@ function predictShift() {
         case "C":
         result = "Shift C ";
         if (cycleDay < 4) {
-            result += `is on day ${cycleDay + 1} of off duty`;
+            result += `(Day ${cycleDay + 1} off duty)`;
         } else if (cycleDay < 8) {
-            result += `is on day ${cycleDay + 1} of off duty`;
+            result += `(Day ${cycleDay + 1} off duty)`;
         } else if (cycleDay < 12) {
-            result += `is on day ${cycleDay - 7} of morning duty`;
+            result += `(Day ${cycleDay - 7} morning duty)`;
         } else {
-            result += `is on day ${cycleDay - 11} of night duty`;
+            result += `(Day ${cycleDay - 11} night duty)`;
         }
         break;
           
         case "D":
         result = "Shift D ";
         if (cycleDay < 4) {
-          result += `is on day ${cycleDay + 1} of morning duty`;
+          result += `(Day ${cycleDay + 1} morning duty)`;
         } else if (cycleDay < 8) {
-          result += `is on day ${cycleDay - 3} of night duty`;
+          result += `(Day ${cycleDay - 3} night duty)`;
         } else {
-          result += `is on day ${cycleDay - 7} of off duty`;
+          result += `(Day ${cycleDay - 7} off duty)`;
         }
         break;
       default:
